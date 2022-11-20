2914b72335268690640943ca54f8e49e752ae99d
theodorostaloumtzis <theodorostaloumtzis@users.noreply.github.com>
2022-11-20 13:08:47 +0200

    new changes

--- Graph.py
+++ Graph.py
@@ -1,13 +1,15 @@
 import Node
+import Edge
+import GraphElement
 import pygame
 import Colour
 
 class Graph:
-    def __init__(self, nodes=None):
-        if nodes is None:
-            self.nodes = []
+    def __init__(self, graph_elements=None):
+        if graph_elements is None:
+            self.graph_elements = []
         else:
-            self.nodes = nodes
+            self.graph_elements = graph_elements
 
     def add_node(self, row, col, width, total_rows, neighbours=None):
         self.nodes.append(Node(row, col, width, total_rows, neighbours))
@@ -15,30 +17,26 @@ class Graph:
     '''Return True if the node with the given value exists. Otherwise it returns False'''
 
     def find_node(self, row, col):
-        for node in self.nodes:
-            if node.col == col and node.row == row:
-                return node
-        return None
+        pass
 
     '''Add a new edge between two nodes'''
-
-    def add_edge(self, value1, value2, weight=1):
-        node1 = self.find_node(value1[0], value1[1])
-        node2 = self.find_node(value2[0], value2[1])
-
-        if (node1 is not None) and (node2 is not None):
-            node1.add_neighboor((node2, weight))
-            node2.add_neighboor((node1, weight))
+    def add_edge(self, value1, value2):
+        pass
 
     ''' Makes the graph '''
     def make_graph(self, rows, width):
         total_rows = 2 * rows - 1
         gap = width // total_rows
         for i in range(total_rows):
-            self.nodes.append([])
+            self.graph_elements.append([])
             for j in range(total_rows):
                 if i % 2 == 0 and j % 2 == 0:
-                    self.nodes[i].append(Node(i, j, gap, total_rows))
+                    self.graph_elements[i].append(Node(i, j, gap, total_rows))
+                elif i % 2 == 1 and j % 2 == 1:
+                    self.graph_elements.append(GraphElement(i, j , gap, total_rows))
+                else:
+                    self.graph_elements.append(Edge(i, j, gap, total_rows))
+
 
     ''' Draws the graph '''
     def draw_graph(self, win, rows, width):
--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from queue import PriorityQueue
 import pygame
 import Node
 import Graph
+import GraphElement
 
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("UCS Path Finding Algorithm")
+pygame.display.set_caption(" Path Finding Algorithm Program")
+
+
+
+
+def main(win, width):
+    
 
 
 
